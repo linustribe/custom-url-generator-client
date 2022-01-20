791d5f7199e49982a93f97e0c8d2e684296eb976
2711-bharath <2711bharath@gmail.com>
2022-01-20 16:12:48 +0530

    feat: created navbar for project and added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/views/home/home.component';
+import { LoginComponent } from './components/views/login/login.component';
+import { RegisterComponent } from './components/views/register/register.component';
+import { MyAccountComponent } from './components/views/my-account/my-account.component';
+import { GenerateUrlComponent } from './components/views/generate-url/generate-url.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'my-account', component: MyAccountComponent },
+  { path: 'generate-url', component: GenerateUrlComponent },
+  { path: '',   redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
