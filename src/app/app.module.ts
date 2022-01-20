f0b292571fdb45d7e2566d5b840f945142a273f5
2711-bharath <2711bharath@gmail.com>
2022-01-20 15:00:30 +0530

    Project Structure initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { LoginComponent } from './components/views/login/login.component';
+import { RegisterComponent } from './components/views/register/register.component';
+import { MyAccountComponent } from './components/views/my-account/my-account.component';
+import { GenerateUrlComponent } from './components/views/generate-url/generate-url.component';
+import { NavbarComponent } from './components/utils/navbar/navbar.component';
+import { FooterComponent } from './components/utils/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    MyAccountComponent,
+    GenerateUrlComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
