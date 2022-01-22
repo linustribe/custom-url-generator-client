a8dbeadd1349e1181b51c2bfcd2a100e9639f093
2711-bharath <2711bharath@gmail.com>
2022-01-22 18:25:56 +0530

    feat: Added router guards and changed navigation accordingly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GenerateUrlComponent, MyAccountComponent, RegisterComponent, HomeComponent, LoginComponent } from './components';
+import { AuthGuard } from './guards/auth.guard';
+import { UserGuard } from './guards/user.guard';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'my-account', component: MyAccountComponent },
-  { path: 'generate-url', component: GenerateUrlComponent },
+  { path: 'login', component: LoginComponent, canActivate: [UserGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [UserGuard] },
+  { path: 'my-account', component: MyAccountComponent, canActivate: [AuthGuard] },
+  { path: 'generate-url', component: GenerateUrlComponent, canActivate: [AuthGuard] },
   { path: '',   redirectTo: 'home', pathMatch: 'full' }
 ];
 
